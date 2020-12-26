b9e9d0c953163b3d90be7001a07a09b688c0fa48
jim <zhoufuhao_java@163.com>
2020-12-26 18:24:00 +0800

    数据排序

--- scripts/qualifiedbox/qualifiedbox_model.js
+++ scripts/qualifiedbox/qualifiedbox_model.js
@@ -11,7 +11,8 @@
         $(".top_info_goods_num").html(data.length);
         let strHTML = "";
         data.forEach((item) => {
-            strHTML += `<li>
+            //                  上市时间           人气                 价格            销售
+            strHTML += `<li time="${item.time}" hot="${item.hot}" price_height="${item.price}" price_low="${item.price}" sales_num="${item.sales_num}">
                             <img src="${item.img}" alt="">
                             <p>${item.title}</p>
                             <p>${item.href}</p>
@@ -26,5 +27,53 @@
         $(".goods_show_lists").html(strHTML);
     }
 
-    // console.log($(".goods_show_lists li").length);
+    $("#sorts_name").change(() => {
+        // 排序按钮
+        let $selectVal = $("option:selected").val();
+        console.log($selectVal == "price_height");
+        let $lis = $(".goods_show_lists li");
+        // 上市时间
+        if ($selectVal == "time") {
+            $lis.sort((m, n) => {
+                return m.getAttribute($selectVal).replace(/-/g, '') - n.getAttribute($selectVal).replace(/-/g, '');
+            });
+
+        }else{
+            $lis.sort((m, n) => {
+                return m.getAttribute($selectVal) - n.getAttribute($selectVal);
+            });
+        }
+        // // 销售数量
+        // if ($selectVal == "sales_num") {
+        //     $lis.sort((m, n) => {
+        //         return m.getAttribute($selectVal) - n.getAttribute($selectVal);
+        //     });
+        // }
+        // // 价格从低到高
+        // if ($selectVal == "price_low") {
+        //     $lis.sort((m, n) => {
+        //         return m.getAttribute($selectVal) - n.getAttribute($selectVal);
+        //     });
+        // }
+        // // 价格从高到低
+        // if ($selectVal == "price_height") {
+        //     $lis.sort((m, n) => {
+        //         return n.getAttribute($selectVal) - m.getAttribute($selectVal);
+        //     });
+        // }
+        // // 人气
+        // if ($selectVal == "hot") {
+        //     $lis.sort((m, n) => {
+        //         return n.getAttribute($selectVal) - m.getAttribute($selectVal);
+        //     });
+        // }
+
+        // 优化回流
+        let frg = document.createDocumentFragment();
+
+        for (let j = 0; j < $lis.length; j++) {
+            frg.appendChild($lis[j]);
+        }
+        $(".goods_show_lists").html(frg);
+    });
 })()
