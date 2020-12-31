aaed4d7140d5113ab335ea3e1de7815718f4e8c4
jim <zhoufuhao_java@163.com>
2020-12-31 23:56:26 +0800

    全选全不选

--- scripts/qualifiedbox/buy_now_model.js
+++ scripts/qualifiedbox/buy_now_model.js
@@ -15,16 +15,38 @@
         });
     });
 
+    // 全选
+    $("input[name='select_all']").on("click", function () {
+        if ($(this).prop("checked")) {
+            $("input[name='single_select']").prop({
+                checked: true
+            });
+        } else {
+            $("input[name='single_select']").prop({
+                checked: false
+            });
+        }
+        statisticNum();
+    });
 
+    // 单选
+    $("input[name='single_select']").on("click", function () {
+        // console.log("单选");
+        if ($(this).prop("checked")) {
+            statisticNum();
+        }
+    });
+
+    // 点击加减按钮进行商品加减
     $(".tab_lists button").on("click", function () {
         // 点击button按钮时所在的索引位置
         let index = $(this).index();
         // 获取商品数量
         let $goods_num = $(this).siblings("input").val();
         // 商品单价
-        let unitPrice = $(this).parent().parent().prev().html();
+        let $unitPrice = $(this).parent().parent().prev().html();
         // 商品单价处理 ￥ 435 只取数字
-        let unitPriceNum = parseFloat(unitPrice.slice(2, unitPrice.length));
+        let unitPriceNum = parseFloat($unitPrice.slice(2, $unitPrice.length));
         // 0为减号  2为加号
         if (index == 0) {
             $goods_num--;
@@ -42,14 +64,19 @@
         statisticNum();
     });
 
-    // 商品数量总计
+    // 只有商品被选中的时候，商品数量开始总计
     let statisticNum = () => {
         let num = 0;
-        $("input[name='goods_num']").each(function () {
-            num += parseFloat($(this).val());
+        $("input[name='single_select']").each(function () {
+            if ($(this).prop("checked")) {
+                let $goods_num = $(this).parent("td").siblings().eq(3).children().find("input").val();
+                num += parseFloat($goods_num);
+            }
         });
+        
         $(".total_num span").html(num);
     }
     statisticNum();
+
 
 })()
